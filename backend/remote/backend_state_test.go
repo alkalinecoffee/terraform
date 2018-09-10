13c08dfa30527ea0bdfb4492872dff48471b77a0
Sander van Harmelen <sander@xanzy.io>
2018-09-10 15:31:18 +0200

    backend/remote: add support for state locking

--- backend/remote/backend_state_test.go
+++ backend/remote/backend_state_test.go
@@ -3,6 +3,7 @@ package remote
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform/backend"
 	"github.com/hashicorp/terraform/state/remote"
 )
 
@@ -13,4 +14,20 @@ func TestRemoteClient_impl(t *testing.T) {
 func TestRemoteClient(t *testing.T) {
 	client := testRemoteClient(t)
 	remote.TestClient(t, client)
+}
+
+func TestRemoteClient_stateLock(t *testing.T) {
+	b := testBackendDefault(t)
+
+	s1, err := b.State(backend.DefaultStateName)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	s2, err := b.State(backend.DefaultStateName)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	remote.TestRemoteLocks(t, s1.(*remote.State).Client, s2.(*remote.State).Client)
 }
